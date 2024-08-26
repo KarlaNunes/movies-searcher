e21a84b2572ab960e07123766d8afd5389f3ad3b
KarlaJulyana <karla@solarz.com.br>
2024-08-26 15:26:58 -0300

    :sparkles: handle searching when types in input

--- src/components/Search/index..tsx
+++ src/components/Search/index..tsx
@@ -1,20 +1,44 @@
 
 import { FiSearch } from 'react-icons/fi';
 import styles from './styles.module.css';  
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export function Search() {
-    return (
-        <div className={styles.buttonContainer}>
-            <input 
-              type="search" 
-              name="search-movies" 
-              id="search-movies" 
-              className={styles.input}
-            />
+  const [query, setQuery] = useState('');
+  const router = useRouter();
+  
+  const handleSearch = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (query) {
+      console.log(`filme novo: ${query}`);
+      router.push(`/movies/${query}`);
+    }
+  }
 
-            <button type="submit" className={styles.button}>
-              <FiSearch size={20} />
-            </button>
-        </div>
-    )
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+
+  return (
+      <div className={styles.buttonContainer}>
+          <input 
+            type="search" 
+            name="search-movies" 
+            id="search-movies" 
+            className={styles.input}
+            value={query}
+            onChange={handleInputChange}
+          />
+
+          <button 
+            type="submit" 
+            className={styles.button}
+            onClick={handleSearch}
+          >
+            <FiSearch size={20} />
+          </button>
+      </div>
+  )
 }
